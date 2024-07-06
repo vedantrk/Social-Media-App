4934ef7496c1f724ab01d8e8ab2f7e88aafce3c8
vedant <vedantrk2003@gmail.com>
2024-07-06 20:22:17 +0530

    initial commit

---
+++ client/src/state/index.js
@@ -0,0 +1,56 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  mode: "light",
+  user: null,
+  friends: [],
+  token: null,
+  posts: [],
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.error("user friends non-existent :(");
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      const updatedPosts = state.posts.map((post) => {
+        if (post._id === action.payload.post._id) return action.payload.post;
+        return post;
+      });
+      state.posts = updatedPosts;
+    },
+    deletePost: (state, action) => {
+      const {postId} = action.payload;
+      console.log(postId)
+      return {
+        ...state, // Spread operator to copy existing state
+        posts: state.posts.filter((post) => post._id !== postId),
+      };
+    },
+  },
+});
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost, deletePost } =
+  authSlice.actions;
+export default authSlice.reducer;
